144910797fdd2531f84bc93802ff2969135147fe
Sherzod <thisisotajonov@gmail.com>
2024-05-19 20:27:09 +0200

    add murphy quotes pages

---
+++ script.js
@@ -0,0 +1,15 @@
+const apiUrl = "https://murphy-quotes-0c76523e6ac1.herokuapp.com/api/quotes/";
+
+async function handleClick() {
+    const response = await fetch(apiUrl);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    const quote = data.quote;
+    console.log(quote);
+    document.getElementById("quote-content").innerHTML = quote;
+}
+
+document
+    .getElementById("get-random-quote")
+    .addEventListener("click", handleClick);
